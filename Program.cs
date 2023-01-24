dcbff639dcd7f66568e081aa3a59be6e4a7dd9e9
Thuyho1502 <p2thanhthuy1502@gmail.com>
2023-01-24 21:14:38 +0100

    føste commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        dyr animal= new dyr();
+        animal.Name = "Lucy";
+        animal.Age = 6;
+        animal.species = "Dog";
+        animal.AnimalInfor();
+
+        dyr animal1= new dyr();
+        animal1.Name = "Ala";
+        animal1.Age = 5;
+        animal1.species = "Cat";
+        animal1.AnimalInfor();
+
+
+        dyr animal2= new dyr();
+        animal2.Name = "Bella";
+        animal2.Age = 10;
+        animal2.species = "turtle";
+        animal2.AnimalInfor();
+
+        
+        
+
+
+
+
+
+    }
+}
---
+++ dyr/dyr.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class dyr
+
+{
+
+
+    public string Name;
+    public ushort Age;
+    public string species;
+
+
+
+    public void AnimalInfor()
+    {
+       
+        Console.WriteLine($"Animal Infor: {Name}  is a {species}, and it is {Age} years old");
+
+    }
+    
+
+}
